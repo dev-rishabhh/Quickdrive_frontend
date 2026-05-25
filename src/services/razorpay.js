2eeab8dd6975737fad4251bc81b45c12843b99c8
Rishabh Pandey <rishabhpandey2963@gmail.com>
2026-05-25 12:31:48 +0530

    integrated webhook

--- src/services/razorpay.js
+++ src/services/razorpay.js
@@ -3,18 +3,13 @@ export function openRazorpayPopup({ subscriptionId }) {
     // console.log(subscriptionId);
 
     const rzp = new Razorpay({
-        key: "rzp_test_StTVj0vLAAdSBp",
-        // key: import.meta.env.VITE_VITE_RAZORPAY_KEY,
+        // key: "rzp_test_StTVj0vLAAdSBp",
+        key: import.meta.env.VITE_VITE_RAZORPAY_KEY,
         description: "Upgrade storage limit",
         name: "Quickdrive",
         subscription_id: subscriptionId,
-        // image: "http://localhost:5173/procodrr.png",
-        // notes: {
-        // },
         handler: async function (response) {
-            // const razorpay_subscription_id
             console.log(response);
-            // await subscriptionComplete(response.razorpay_subscription_id)
         },
     });
 
@@ -42,20 +37,5 @@ export async function subscriptionInitiate({ planId }) {
     return data
 }
 
-async function subscriptionComplete(subscriptionId) {
-    const res = await fetch(`${import.meta.env.VITE_BASE_URL}/subscriptions/complete`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            subscriptionId
-        }),
-        credentials: "include"
-    })
-
-    const data = await res.json()
-    return data
-}
 
 
